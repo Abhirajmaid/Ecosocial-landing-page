bf4614cb2e82ebc2bf1593ec3b7bbce0133c7dac
Abhiraj <codersground404@gmail.com>
2022-02-17 11:36:16 +0530

    Fixed Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { CTABtn } from "../CTABtn/CTABtn";
-import { FaTimes } from "react-icons/fa";
-// import logo from "./images/logo.png";
+// import { FaTimes } from "react-icons/fa";
 import "./style.css";
 
 export const Navbar = () => {
@@ -30,38 +29,6 @@ export const Navbar = () => {
             <FaTimes size={15} style={{ color: "black" }} />
           )}
         </div> */}
-        {/* <a href="/" className="logo">
-          <img src={logo} alt="logo" />
-        </a>
-        <div className="hamburger" onClick={handleClick}>
-          {click ? (
-            <FaTimes size={30} style={{ color: "#ffffff" }} />
-          ) : (
-            <FaBars size={30} style={{ color: "#ffffff" }} />
-          )}
-        </div>
-        <ul className={click ? "nav-menu active" : "nav-menu"}>
-          <li className="nav-item">
-            <a href="/" onClick={closeMenu}>
-              Home
-            </a>
-          </li>
-          <li className="nav-item">
-            <a href="#about" onClick={closeMenu}>
-              About
-            </a>
-          </li>
-          <li className="nav-item">
-            <a href="#testimonials" onClick={closeMenu}>
-              Testimonials
-            </a>
-          </li>
-          <li className="nav-item">
-            <a href="#demo" onClick={closeMenu}>
-              Demo
-            </a>
-          </li>
-        </ul> */}
       </nav>
     </div>
   );
